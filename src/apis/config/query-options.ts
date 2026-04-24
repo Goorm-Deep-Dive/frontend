4cca2947f3c8a07e1f4d6ba1fd4186ff71f6681d
Leemainsw <yebom6476@gmail.com>
2026-04-24 10:24:38 +0900

    chore: Orval 생성 useQuery의 기본 TanStack Query 옵션 (#4)

---
+++ src/apis/config/query-options.ts
@@ -0,0 +1,10 @@
+import { UseQueryOptions } from "@tanstack/react-query";
+
+export const customQueryOptions: Partial<UseQueryOptions> = {
+  // 데이터가 'fresh'한 시간 (5분)
+  staleTime: 1000 * 60 * 5,
+  // 윈도우 포커스 시 자동 리페칭 끔
+  refetchOnWindowFocus: false,
+  // 에러 발생 시 재시도 횟수
+  retry: 2,
+};
