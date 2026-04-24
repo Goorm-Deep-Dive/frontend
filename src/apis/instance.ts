780de427ac0cfdacaca1128593d4076a3d64d3b6
Leemainsw <yebom6476@gmail.com>
2026-04-24 10:56:20 +0900

    fix: SSR 환경에서 토큰 조회 가드 추가 (#4)
    요청 인터셉터의 localStorage 접근을 브라우저 환경에서만 수행하도록 보호해 서버 사이드 실행 시 참조 오류를 방지한다.

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -15,8 +15,12 @@ export const axiosInstance: AxiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("accessToken");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("accessToken");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
     return config;
   },
   (error) => Promise.reject(error),
