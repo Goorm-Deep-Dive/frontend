68422fd745b0ed12a1cd5bdab9480ff0c151b14e
Leemainsw <yebom6476@gmail.com>
2026-04-27 05:54:36 +0900

    style: bottom-navigation 스타일 수정 (#26)

--- src/components/common/bottom-navigation/index.tsx
+++ src/components/common/bottom-navigation/index.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { cn } from "@/lib/cn";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function BottomNavigation() {
+  const pathname = usePathname();
+  const isActive = (path: string) => pathname === path;
+
   const MENUS = [
     {
       name: "진행상황",
@@ -11,13 +17,7 @@ export default function BottomNavigation() {
     {
       name: "체크리스트",
       icon: "/icons/bottom-navigation/check.svg",
-
       path: "/checklist",
-    },
-    {
-      name: "스케줄링",
-      icon: "/icons/bottom-navigation/schedule.svg",
-      path: "/schedule",
     },
     {
       name: "마이페이지",
@@ -29,17 +29,20 @@ export default function BottomNavigation() {
   return (
     <nav
       aria-label="바텀 네비게이션"
-      className="fixed bottom-0 z-10 w-full rounded-t-2xl border-t border-[#DADADA] bg-white px-6 pt-4"
+      className="fixed bottom-0 z-10 w-full rounded-t-2xl border-t border-[#DADADA] bg-white"
     >
-      <div className="flex items-center">
+      <div className="flex items-center justify-center gap-2 pb-7">
         {MENUS.map((menu) => (
           <Link
             href={menu.path}
             key={menu.path}
-            className="flex flex-1 flex-col items-center gap-2 pb-10"
+            className={cn(
+              "flex h-20 w-20 flex-1 flex-col items-center justify-center gap-2.5",
+              isActive(menu.path) && "text-primary-1",
+            )}
           >
             <Image src={menu.icon} alt={menu.name} width={24} height={24} />
-            <span className="text-sm text-black">{menu.name}</span>
+            <span className="text-sm">{menu.name}</span>
           </Link>
         ))}
       </div>
