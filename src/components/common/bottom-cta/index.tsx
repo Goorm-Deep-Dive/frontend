487792931e19f771d86edb5bfa7a0306da60e3ae
Leemainsw <yebom6476@gmail.com>
2026-04-28 01:21:22 +0900

    feat: bottom-cta 컴포넌트 추가 (#36)

---
+++ src/components/common/bottom-cta/index.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/cn";
+
+interface Props {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function BottomCTA({ children, className }: Props) {
+  return (
+    <>
+      <div className="h-26" />
+      <div
+        className={cn(
+          "itesm-center fixed right-0 bottom-0 left-0 z-40 mx-auto flex max-w-[var(--app-max-width)] justify-center px-5 pt-5 pb-[calc(env(safe-area-inset-bottom)+20px)]",
+          className,
+        )}
+      >
+        {children}
+      </div>
+    </>
+  );
+}
