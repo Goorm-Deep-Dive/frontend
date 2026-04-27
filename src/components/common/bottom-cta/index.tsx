b91c94220b96f72c175fb995d6a4e6bcc05b388d
Leemainsw <yebom6476@gmail.com>
2026-04-28 02:31:22 +0900

    refactor: 오타 수정 (#33)

--- src/components/common/bottom-cta/index.tsx
+++ src/components/common/bottom-cta/index.tsx
@@ -11,7 +11,7 @@ export default function BottomCTA({ children, className }: Props) {
       <div className="h-26" />
       <div
         className={cn(
-          "itesm-center fixed right-0 bottom-0 left-0 z-40 mx-auto flex max-w-[var(--app-max-width)] justify-center px-5 pt-5 pb-[calc(env(safe-area-inset-bottom)+20px)]",
+          "fixed right-0 bottom-0 left-0 z-40 mx-auto flex max-w-[var(--app-max-width)] items-center justify-center px-5 pt-5 pb-[calc(env(safe-area-inset-bottom)+20px)]",
           className,
         )}
       >
