a3dcb78b402d1c88007407ed7df42a6bc3ad32cd
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2026-05-01 01:39:14 +0900

    #53 feat: 영면일 입력 퍼블리싱 (#55)
    * feat: 영면일 입력 퍼블리싱 (#53)
    
    * feat: 영면일 입력 퍼블리싱 (#53)
    
    * refactor: 리뷰 수정 반영 (#53)

---
+++ src/app/start/page.tsx
@@ -0,0 +1,26 @@
+import Start from "@/app/start/_components/start-content";
+import { Button } from "@/components/ui/button";
+
+export default function StartPage() {
+  return (
+    <div className="mt-26 flex flex-col gap-7.5">
+      <div className="flex justify-between gap-3.5 pr-5 pl-7.5">
+        <div className="flex flex-col gap-2.5">
+          <span className="h1">동행 시작하기</span>
+          <span>기존 회원이시라면, 로그인 해주세요.</span>
+        </div>
+        <div className="flex items-end">
+          <Button
+            size="small"
+            className="rounded-xl bg-gray-300 px-3 py-1 text-gray-900"
+          >
+            로그인 하기
+          </Button>
+        </div>
+      </div>
+      <div>
+        <Start />
+      </div>
+    </div>
+  );
+}
--- src/components/common/bottom-cta/index.tsx
+++ src/components/common/bottom-cta/index.tsx
@@ -11,7 +11,7 @@ export default function BottomCTA({ children, className }: Props) {
       <div aria-hidden className="h-37.5 min-h-37.5" />
       <div
         className={cn(
-          "fixed right-0 bottom-0 left-0 z-40 mx-auto flex h-37.5 max-w-(--app-max-width) items-center justify-center rounded-tl-2xl rounded-tr-2xl bg-gray-200 px-5 pt-5 pb-[calc(env(safe-area-inset-bottom)+20px)]",
+          "fixed right-0 bottom-0 left-0 z-40 mx-auto flex h-37.5 max-w-(--app-max-width) flex-col items-center justify-center gap-2.5 rounded-tl-2xl rounded-tr-2xl bg-gray-200 px-5 pt-5 pb-[calc(env(safe-area-inset-bottom)+20px)]",
           className,
         )}
       >
