dccf63daa0498336f8470d46edbe10228806e66c
GwonWooL <229861@naver.com>
2026-04-30 23:05:19 +0900

    refactor: input 공통 컴포넌트 수정 (#21)

--- src/components/common/input/index.tsx
+++ src/components/common/input/index.tsx
@@ -1,19 +1,20 @@
-import { useState } from "react";
+interface InputProps {
+  value: string;
+  onChange: (value: string) => void;
+}
 
-export default function Input() {
-  const [value, setValue] = useState("");
+export default function Input({ value, onChange }: InputProps) {
   const isFilled = value.length > 0;
-
   return (
-    <div
-      className={`h3 caret-primary-1 bg-primary-bg flex justify-between rounded-lg px-2.5 py-2.5 ${isFilled ? "text-primary-1" : "text-primary-2"}`}
-    >
-      <input
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        placeholder="성명 또는 명칭"
-        className={`outline-none`}
-      />
+    <div className="rounded-lg bg-gray-200 p-5">
+      <div className="bg-primary-10 rounded-lg px-5 py-2.5">
+        <input
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          placeholder="성명 또는 명칭"
+          className={`h3 focus:text-primary-1 outline-none ${isFilled ? "text-primary-1" : "text-primary-2"}`}
+        />
+      </div>
     </div>
   );
 }
