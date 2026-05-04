b7fd603e001a7e95d25b416642d0542695689cdb
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2026-05-04 10:12:55 +0900

    #51 feat: 체크리스트 메인화면 퍼블리싱  (#60)
    * feat: 체크리스트 메인화면 퍼블리싱 (#51)
    
    * feat: checklist-header-card 컴포넌트 분리 (#51)
    
    * feat: linear bg 추가 (#51)
    
    * refactor: 네이밍 수정 (#51)
    
    ---------
    
    Co-authored-by: Lee_yam <48716775+Leemainsw@users.noreply.github.com>
    Co-authored-by: Leemainsw <yebom6476@gmail.com>

---
+++ src/app/checklist/page.tsx
@@ -0,0 +1,55 @@
+import ChecklistList from "./_components/checklist-list";
+import ChecklistHeaderCard from "./_components/checklist-header-card";
+import Header from "@/components/common/header";
+import BottomNavigation from "@/components/common/bottom-navigation";
+
+type Status = "default" | "urgent" | "done";
+
+interface ChecklistPageProps {
+  date: string;
+  remainingCount: number;
+}
+
+const checklistData = [
+  { title: "Title1", status: "default", listDate: "D-30" },
+  { title: "Title2", status: "default", listDate: "D-30" },
+  { title: "Title3", status: "done" },
+  { title: "Title4", status: "done" },
+];
+
+export default function ChecklistPage({
+  date = "D-30",
+  remainingCount = 3,
+}: ChecklistPageProps) {
+  return (
+    <>
+      <Header variant="checklist" />
+      <div className="flex w-full flex-col">
+        <div className="flex w-full flex-col gap-5">
+          <div className="bg-tab-bg flex justify-center gap-2.5 py-1.5">
+            <span className="label">가장 빠른 기한까지 </span>
+            <span className="text-sementic-red">{date}</span>
+            <span className="label">일 남았어요</span>
+          </div>
+
+          <div className="flex flex-col justify-center gap-5 px-5">
+            <ChecklistHeaderCard profile="Profile1" count={remainingCount} />
+
+            <div className="flex flex-col gap-2.5">
+              {checklistData.map((item, index) => (
+                <ChecklistList
+                  key={item.title}
+                  title={item.title}
+                  index={index + 1}
+                  status={item.status as Status}
+                  listDate={item.listDate}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <BottomNavigation />
+    </>
+  );
+}
