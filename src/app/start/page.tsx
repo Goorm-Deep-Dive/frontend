43e55adbf9606404dc621391cf295698525f236d
GwonWooL <229861@naver.com>
2026-05-07 15:36:43 +0900

    refactor: 화면 비율 수정, 현재 날짜 이후 입력 불가능, 캘린더 크기 수정 (#71)

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -3,22 +3,28 @@ import AddForm from "./_components/add-form";
 
 export default function Page() {
   return (
-    <div className="mt-26 flex flex-col gap-7.5">
-      <div className="flex justify-between gap-3.5 pr-5 pl-7.5">
+    <div className="flex flex-col gap-7.5">
+      <div className="mt-26 pl-7.5">
         <div className="flex flex-col gap-2.5">
-          <span className="h1">동행 시작하기</span>
-          <span>기존 회원이시라면, 로그인 해주세요.</span>
-        </div>
-        <div className="flex items-end">
-          <Button
-            size="small"
-            className="rounded-xl bg-gray-300 px-3 py-1 text-gray-900"
-          >
-            로그인 하기
-          </Button>
+          <span className="h1 text-gray-900">동행 시작하기</span>
+          <div className="flex items-end gap-3.5">
+            <span className="font-regular text-[1rem] tracking-[-0.02em] text-gray-700">
+              기존 회원이시라면, 로그인 해주세요.
+            </span>
+            <Button
+              size="small"
+              className="label rounded-[20px] bg-gray-300 px-3 py-1 text-gray-700"
+            >
+              로그인 하기
+            </Button>
+          </div>
         </div>
       </div>
-      <AddForm />
+      <div className="rounded-t-[20px] border-t-2 shadow-[0px_-2px_2px_rgba(0,0,0,0.01)]">
+        <div className="mt-15">
+          <AddForm />
+        </div>
+      </div>
     </div>
   );
 }
