d93d7ec9c80d0cbf9f5331793b5cbf647eeaa4f6
Leemainsw <yebom6476@gmail.com>
2026-05-19 11:19:26 +0900

    refactor: 일정 페이지 공통 Header 컴포넌트 적용 (#108)

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import CalendarHeader from "./_components/calendar-header";
 import Calendar from "./_components/calendar";
 import CalendarTag from "./_components/calendar-tag";
 import CalendarSchedule from "./_components/calendar-schedule";
 import CalendarNotSchedule from "./_components/calendar-not-schedule";
 import BottomNavigation from "@/components/common/bottom-navigation";
 import { useState } from "react";
+import Header from "@/components/common/header";
 
 type Mode = "monthly" | "weekly" | "daily";
 
@@ -14,21 +14,21 @@ export default function SchedulePage() {
   const [mode, setMode] = useState<Mode>("daily");
 
   return (
-    <div className="flex w-full flex-col gap-10.5">
-      <div className="border-b border-gray-200 pt-11">
-        <CalendarHeader />
-      </div>
-      <div className="flex flex-col gap-10 px-5">
-        <div className="flex w-full flex-col gap-2.5">
-          <Calendar />
-          <CalendarTag />
-        </div>
-        <div className="flex flex-col gap-10">
-          <CalendarSchedule mode={mode} setMode={setMode} />
-          <CalendarNotSchedule />
+    <>
+      <Header title="일정 관리" variant="default" />
+      <div className="mt-3 flex w-full flex-col gap-10.5">
+        <div className="flex flex-col gap-10 px-5">
+          <div className="flex w-full flex-col gap-2.5">
+            <Calendar />
+            <CalendarTag />
+          </div>
+          <div className="flex flex-col gap-10">
+            <CalendarSchedule mode={mode} setMode={setMode} />
+            <CalendarNotSchedule />
+          </div>
         </div>
       </div>
       <BottomNavigation />
-    </div>
+    </>
   );
 }
