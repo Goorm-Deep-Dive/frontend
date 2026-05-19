e1fc60cc40a19948ac26830ac35eac984868f14e
Leemainsw <yebom6476@gmail.com>
2026-05-19 16:12:04 +0900

    feat: 진행상황 카테고리별 아이콘 및 프로그레스바 색상 적용 (#120)

--- src/app/progress/_components/category-card.tsx
+++ src/app/progress/_components/category-card.tsx
@@ -1,5 +1,36 @@
-import { PcCase } from "lucide-react";
 import ProgressBar from "@/components/common/progress-bar";
+import ChartDocumentIcon from "@/components/icons/chart-document-icon";
+import ChartDocumentCourtNoticeIcon from "@/components/icons/chart-document-court-notice-icon";
+import ChartDocumentDigitalIcon from "@/components/icons/chart-document-digital-icon";
+import ChartDocumentFinanceIcon from "@/components/icons/chart-document-finance-icon";
+
+function getIcon(name: string) {
+  switch (name) {
+    case "사무행정":
+      return <ChartDocumentIcon className="h-6 w-6" />;
+    case "금융":
+      return <ChartDocumentFinanceIcon className="h-6 w-6" />;
+    case "디지털":
+      return <ChartDocumentDigitalIcon className="h-6 w-6" />;
+    case "법원행정":
+      return <ChartDocumentCourtNoticeIcon className="h-6 w-6" />;
+  }
+}
+
+function getColor(name: string) {
+  switch (name) {
+    case "사무행정":
+      return "bg-tag-office";
+    case "금융":
+      return "bg-tag-digital";
+    case "디지털":
+      return "bg-tag-finance";
+    case "법원행정":
+      return "bg-tag-court";
+    default:
+      return "bg-primary-1";
+  }
+}
 
 interface Props {
   name: string;
@@ -17,7 +48,7 @@ export default function CategoryCard({
     <button className="bg-tab-bg flex flex-col gap-2 rounded-md px-2.5 py-3 shadow-sm">
       <div className="flex items-center justify-between">
         <div className="flex items-center justify-start gap-1.5">
-          <PcCase />
+          {getIcon(name)}
           <span className="h4">{name}</span>
         </div>
 
@@ -25,7 +56,7 @@ export default function CategoryCard({
           {completedCount}/{totalCount} 완료
         </span>
       </div>
-      <ProgressBar value={progressRate} />
+      <ProgressBar value={progressRate} indicatorClassName={getColor(name)} />
     </button>
   );
 }
