e69a413825cfff70bdce3201f62c29c3af5be7b1
GwonWooLee <143773589+GwonWooL@users.noreply.github.com>
2026-05-20 09:59:58 +0900

    fix: 체크리스트 모달 모바일 스크롤 및 max-height 대응 및 start 화면 로그인 기능 제거 (#126) (#127)

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -7,17 +7,6 @@ export default function Page() {
       <div className="mt-26 pl-7.5">
         <div className="flex flex-col gap-2.5">
           <span className="h1 text-gray-900">동행 시작하기</span>
-          <div className="flex items-end gap-3.5">
-            <span className="font-regular text-[1rem] tracking-[-0.02em] text-gray-700">
-              기존 회원이시라면, 로그인 해주세요.
-            </span>
-            <Button
-              size="small"
-              className="label rounded-[1.25rem] bg-gray-300 px-3 py-1 text-gray-700"
-            >
-              로그인 하기
-            </Button>
-          </div>
         </div>
       </div>
       <div className="rounded-t-[1.25rem] border-t-2 shadow-[0px_-2px_2px_rgba(0,0,0,0.01)]">
